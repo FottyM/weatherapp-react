92889abca03ff5c122189f2baf91f19d642144b3
Fortunat Mutunda <fotimutunda@gmail.com>
2018-01-02 02:31:16 +0200

    render everything properly now unto details and cleanup

--- src/containers/Result.js
+++ src/containers/Result.js
@@ -24,37 +24,37 @@ class Result extends Component {
 
     return (
       <table>
-        <tr>
-          <td>Morning</td>
-          <td>
-            {data[3].morn} {this.symbol()}
-          </td>
-        </tr>
-        <tr>
-          <td>Day</td>
-          <td>
-            {' '}
-            {data[0].day} {this.symbol()}
-          </td>
-        </tr>
-        <tr>
-          <td>Evening</td>
-          <td>
-            {data[2].eve} {this.symbol()}{' '}
-          </td>
-        </tr>
-        <tr>
-          <td>Night</td>
-          <td>
-            {data[1].night} {this.symbol()}
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Morning</td>
+            <td>
+              {data[3].morn} {this.setSymbol()}
+            </td>
+          </tr>
+          <tr>
+            <td>Day</td>
+            <td>
+              {data[0].day} {this.setSymbol()}
+            </td>
+          </tr>
+          <tr>
+            <td>Evening</td>
+            <td>
+              {data[2].eve} {this.setSymbol()}{' '}
+            </td>
+          </tr>
+          <tr>
+            <td>Night</td>
+            <td>
+              {data[1].night} {this.setSymbol()}
+            </td>
+          </tr>
+        </tbody>
       </table>
     );
   }
 
-  setDay() {
-    let date = moment().format('MMMM Do YYYY, h:mm:ss a');
+  setDay(date) {
     let today = new Date();
     today = today.setDate(today.getDate() + date);
     today = new Date(today).getDay();
@@ -70,28 +70,40 @@ class Result extends Component {
     return days[today];
   }
 
-  symbol() {
+  setSymbol() {
     if (this.props.unitOfMeasure === 'f') {
       return renderHTML('&#8457;'); //degree F
     }
     return renderHTML('&#8451;'); //degree C
   }
 
-  dayAndNightShift() {
-    let currentTime = new Date();
-    let hours = currentTime.getHours();
+  showIcondayAndNightShift(code) {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const prefix = 'wi wi-owm';
+    let icon = '';
+    if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+      if (hours < 6 || hours >= 18) {
+        return `${prefix}-night-${code}`;
+      }
 
-    if (hours < 6 || hours >= 18) return 'night-';
-
-    if (hours >= 6 && hours < 18) return 'day-';
+      if (hours >= 6 && hours < 18) {
+        return `${prefix}-day-${code}`;
+      }
+    }
+    icon = prefix + '-' + code;
+    return icon;
   }
 
-  renderSevenDaysForecast(i) {
+  renderSevenDaysForecast(el, i) {
+    const id = el.weather[0].id;
     return (
       <div key={i}>
-        <p>Tue</p>
-        <p>Logo</p>
-        <p>Temp</p>
+        <p>{this.setDay(i)}</p>
+        <p className="bigger-icon">
+          <i className={this.showIcondayAndNightShift(id)} />
+        </p>
+        <p>{`${el.temp.day} ${this.setSymbol()}`}</p>
       </div>
     );
   }
@@ -112,7 +124,6 @@ class Result extends Component {
       unit = this.props.unitOfMeasure,
       data = this.changedUnitOfMeasure(unit),
       sevenDaysForecast = data.generalData.list;
-    // let dailyForecst = data.generalData.list[0].temp
 
     return (
       <div className="container">
@@ -129,14 +140,15 @@ class Result extends Component {
           <h2> {this.capitalize(data.specificData.weather[0].description)} </h2>
         </div>
         <div className="big-temp orange">
-          {' '}
-          <h2>
-            {data.specificData.main.temp} {this.symbol()}
-          </h2>
+          <h2>{`${data.specificData.main.temp} ${this.setSymbol()}`}</h2>
         </div>
         <div className="big-icon orange">
           <h2>
-            <i className={`wi wi-owm-${data.specificData.weather[0].id}`} />
+            <i
+              className={this.showIcondayAndNightShift(
+                data.specificData.weather[0].id
+              )}
+            />
           </h2>
         </div>
         <div className="daily-forecast orange">
@@ -144,7 +156,7 @@ class Result extends Component {
         </div>
         <div className="days-forecast">
           {sevenDaysForecast.map((el, index) =>
-            this.renderSevenDaysForecast(index)
+            this.renderSevenDaysForecast(el, index)
           )}
         </div>
       </div>
